c19d25329f712ca4bd27ba4e6c51d1083ef80a1e
Ulf <ulf.rustas@gmail.com>
2018-02-01 20:58:08 +0100

    Start work on Score counter. Is not shown yet.

--- rally.asm
+++ rally.asm
@@ -27,6 +27,7 @@
 
         seg.u Variables
         org $80
+Temp        .byte
         
 xpos    .byte #0
 speedi  .byte #0
@@ -37,9 +38,18 @@ zone    .byte #0
 fueli    .byte #0
 fuelf    .byte #0
 fuellines .byte #0
-
-
 rowcounter .byte #0
+
+; Pointers to bitmap for each digit
+Digit0      .word
+Digit1      .word
+Digit2      .word
+Digit3      .word
+Digit4      .word
+Digit5      .word
+
+BCDScore    hex 000000
+LoopCount   .byte ; counts scanline when drawing
 
         seg Code
         org $f000                
@@ -54,18 +64,32 @@ Crash
         sta fueli
         lda #$FF
         sta fuelf
+        
+; ----------------------------------------------
+; Vertical Sync
+; ----------------------------------------------
+
 NextFrame
         VERTICAL_SYNC
+        
+; ----------------------------------------------
+; Vertical Blank
+; ----------------------------------------------
+
         TIMER_SETUP 37
 
 ; Clear sprite
+; ---------------------
         lda $0
         sta GRP0
+        
 ; Set Sprite Color
+; ---------------------
         lda $1A
         sta COLUP0
         
 ; Read joystick left right
+; ---------------------
         ldx xpos
         beq NotMovingLeft
         bit SWCHA
@@ -81,6 +105,8 @@ NotMovingRight
         stx xpos
         
 ; Read joystick up/down
+; ---------------------
+
     lda speedi
         cmp #4
         beq NotAccelerating
@@ -118,6 +144,7 @@ Done
         sta disti
         
 ; Update Fuel
+; --------------
         lda fuelf
         sec
         sbc #$04
@@ -131,31 +158,16 @@ FuelLeft
     sta fueli
         
 
-; Set Sprite Pos
-        txa
-        sec
-        sta WSYNC
-        sta HMCLR
-DivideLoop
-        sbc #15
-        bcs DivideLoop
-
-        eor #7
-        asl
-        asl
-        asl
-        asl
-        sta HMP0
-        sta RESP0 
-        sta WSYNC
-        sta HMOVE
-
+; Setup rows for frame
+; --------------
         ldx #0
         lda disti
         sta rowcounter
         
 ;        SKIP_SCANLINES 150
 
+; Setup Fuelmeter
+; --------------
         lda fueli
         asl
         adc fueli ; fuellines < 128, no need to clc
@@ -175,24 +187,55 @@ DivideLoop
         sta COLUP1
         
         TIMER_WAIT      
+        
+; ----------------------------------------------
+; Visible frame
+; ----------------------------------------------
+
+; FuelMeter
+; ---------
+
         lda #8
         sta fuellines
-FuelMeter
+FuelMeterLoop
         sta WSYNC                
         dec fuellines
-        bne FuelMeter
+        bne FuelMeterLoop
         
         lda #0
         sta PF0
         sta PF1
         sta PF2
         
+; Set Sprite Pos
+; --------------
+        lda xpos
+        sec
+        sta WSYNC
+        sta HMCLR
+DivideLoop
+        sbc #15
+        bcs DivideLoop
+
+        eor #7
+        asl
+        asl
+        asl
+        asl
+        sta HMP0
+        sta RESP0 
+        sta WSYNC
+        sta HMOVE
+
+; Playfield Before Sprite
+; -----------------------
+
 BeforeSpriteLoop
         BACKGROUND_LINES
         sta WSYNC
         sta COLUBK
         inx
-        cpx #141
+        cpx #143
         bcc BeforeSpriteLoop
         
         
@@ -201,7 +244,10 @@ BeforeSpriteLoop
         dey
         BACKGROUND_LINES
         
+; Sprite
+; -----------------------
 SpriteLoop
+
         sta WSYNC       
         stx GRP0
         sta COLUBK
@@ -212,6 +258,9 @@ SpriteLoop
         
         dey
         bpl SpriteLoop
+
+; Playfield After Sprite
+; -----------------------
 
         sta WSYNC       
         stx GRP0
@@ -228,11 +277,116 @@ AfterSpriteLoop
         ldx #0
         stx COLUBK
 
+; ----------------------------------------------
+; Overscan
+; ----------------------------------------------
+
         TIMER_SETUP 30
+        lda #01
+        ldx #00
+        ldy #00
+        jsr AddScore
         TIMER_WAIT      
-        sta WSYNC
-        
         jmp NextFrame
+        
+        
+; ==============================================        
+; Subroutines
+; ==============================================        
+
+; Adds value to 6-BCD-digit score.
+; A = 1st BCD digit
+; X = 2nd BCD digit
+; Y = 3rd BCD digit
+AddScore subroutine
+        sed ; enter BCD mode
+        clc ; clear carry
+        sta Temp
+        lda BCDScore
+        adc Temp
+        sta BCDScore
+        stx Temp
+        lda BCDScore+1
+        adc Temp
+        sta BCDScore+1
+        sty Temp
+        lda BCDScore+2
+        adc Temp
+        sta BCDScore+2
+        cld ; exit BCD mode
+        rts
+        
+GetDigitPtrs subroutine
+    ldx #0  ; leftmost bitmap
+        ldy #2  ; start from most-sigificant BCD value
+.Loop
+        lda BCDScore,y  ; get BCD value
+        and #$f0    ; isolate high nibble (* 16)
+        lsr     ; shift right 1 bit (* 8)
+        sta Digit0,x    ; store pointer lo byte
+        lda #>FontTable
+        sta Digit0+1,x  ; store pointer hi byte
+        inx
+        inx     ; next bitmap pointer
+        lda BCDScore,y  ; get BCD value (again)
+        and #$f     ; isolate low nibble
+        asl
+        asl
+        asl     ; * 8
+        sta Digit0,x    ; store pointer lo byte
+        lda #>FontTable
+        sta Digit0+1,x  ; store pointer hi byte
+        inx
+        inx     ; next bitmap pointer
+        dey     ; next BCD value
+        bpl .Loop   ; repeat until < 0
+    rts
+
+; Display the resulting 48x8 bitmap
+; using the Digit0-5 pointers.
+DrawDigits subroutine
+    sta WSYNC
+    SLEEP 40    ; start near end of scanline
+        lda #7
+        sta LoopCount
+BigLoop
+    ldy LoopCount   ; counts backwards
+        lda (Digit0),y  ; load B0 (1st sprite byte)
+        sta GRP0    ; B0 -> [GRP0]
+        lda (Digit1),y  ; load B1 -> A
+        sta GRP1    ; B1 -> [GRP1], B0 -> GRP0
+        sta WSYNC   ; sync to next scanline
+        lda (Digit2),y  ; load B2 -> A
+        sta GRP0    ; B2 -> [GRP0], B1 -> GRP1
+        lda (Digit5),y  ; load B5 -> A
+        sta Temp    ; B5 -> temp
+        lda (Digit4),y  ; load B4
+        tax     ; -> X
+        lda (Digit3),y  ; load B3 -> A
+        ldy Temp    ; load B5 -> Y
+        sta GRP1    ; B3 -> [GRP1]; B2 -> GRP0
+        stx GRP0    ; B4 -> [GRP0]; B3 -> GRP1
+        sty GRP1    ; B5 -> [GRP1]; B4 -> GRP0
+        sta GRP0    ; ?? -> [GRP0]; B5 -> GRP1
+        dec LoopCount   ; go to next line
+    bpl BigLoop ; repeat until < 0
+        
+        lda #0      ; clear the sprite registers
+        sta GRP0
+        sta GRP1
+        sta GRP0
+        sta GRP1
+        rts
+
+; Font table for digits 0-9 (8x8 pixels)
+        align $100 ; make sure data doesn't cross page boundary
+FontTable
+    hex 003c6666766e663c007e181818381818
+        hex 007e60300c06663c003c66061c06663c
+        hex 0006067f661e0e06003c6606067c607e
+        hex 003c66667c60663c00181818180c667e
+        hex 003c66663c66663c003c66063e66663c
+        
 
         align $100
 SpriteData
